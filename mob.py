09df5f398a3632a2be49c90a675eb7252d6ec773
Joe McCune <joe.mccune@faithlife.com>
2021-05-26 12:23:59 -0700

    Improve Printing of Results

--- mob.py
+++ mob.py
@@ -1,10 +1,10 @@
-# add in regex for Android and Bep
-# regex for Fire OS and Android- date time format is different, examples of diff syntax in fire.zip and android.zip
-# TO DO Piar all results with date stamps
+# TO DO 
 # get rid of print statemtnets that call out which regex found this result
-# In Android, the "launched" info is spread out over several different lines
 # Consolidate rx for iOS date time and BEP iOS date time
-# make allowance for Android message beginning of crash with the actual crash on the next line
+# add BEP crash
+# Printing the results is a hot mess
+# Preferred Bible regex needs refinement
+# regex needs to be added for problems downloading books
 
 
 from zipfile import ZipFile
@@ -22,7 +22,14 @@ os_v = []
 lang = []
 user_id = []
 preferred = []
-reading_plans = []
+android_reading_plans = []
+crash = []
+
+rez = (
+        f'Os: {os_name} {os_v}\nApp: {app_name} {app_v}\nDevice: {device}\nLanguage: {lang}\nUser ID: {user_id}\n'
+        f'Preferred Bible(s): {preferred}\nDetected Reading Plans: \n{android_reading_plans}\n'
+        f'{len(crash)} Crash(es):\n{crash}'
+        ) 
 
 
 def findzip():
@@ -56,14 +63,12 @@ def unzip_iter():
     with ZipFile(logzip, 'r') as zipObj:
         list_of_fileNames = zipObj.namelist()
         print(list_of_fileNames)
-        print('STARTING\n\n')
         for file_name in list_of_fileNames:
             if file_name.endswith('.log') or file_name.endswith('.txt'):
                 zipObj.extract(file_name)
                 print(f'\nCurrent Log: {file_name}\n')
                 tally = get_info(file_name)
-    print(tally)
-    print('\n\nFINISHED')
+    organize_results(tally)
 
 def add_if_new(item, item_list):
     if item not in item_list:
@@ -80,14 +85,9 @@ def get_info(each_iter_file):
     android_launched_rx = '.+Application\slaunched\s/\s([0-9]+)$'
     user1_rx = 'userId=\s?([0-9]+)&'
     user2_rx = 'UserID=([0-9]+)"'
-    # need to get another speciman to see what correct regex will be, it might start with time date ^^^
     user_sup_rx = '.+\sSupport/Users/([0-9]+)/'
-    # lls1_rx = '.+new items:\s\[{\s([0-9]+)/.+'
     lls1_rx = '.+items?:\s\[?\{?\s?([0-9]+)/.+'
     lls2_rx = '.+synced:\s([0-9]+)/.+'
-    # android_lls_rx = '\s([0-9]+)/LLS:'
-    # needs further refinement ^^^
-    # android_mod_rx = '.+state\(Modified\)'
     android_mod_rx = '.+state\(Modified\):\s([0-9]+)/'
     fire_items_rx = '.+items:\s\[\{"id":"([0-9]+)/'
     anon_rx = '.+User\sis\s(anonymous)'
@@ -95,14 +95,23 @@ def get_info(each_iter_file):
     bep_android_specs_rx = 'I/DeveloperSendLogsEmail\([0-9]+\):\s(.+)$'
     bep_ios_specs_rx = '.+I/DeveloperSendLogsEmail:\s(.+)$'
     bep_appsup_rx = '.+/Library/Application Support/([0-9]+)/'
+    android_crash_rx = '.+beginning\sof\scrash$'
+    ios_crash_rx = '.+Crash detected.+'
     android_lang_rx = '.+DeviceLanguage:([\w]+)$'
-    pref_bib_rx = '.+preferredBible\?resourceId=LLS%3A([.\w]+)$'
+    ios_pref_bib_rx = '.+preferredBible\?resourceId=LLS%3A([.\w]+)$'
+    android_pref_bib1_rx = '.+PreferredBible:LLS:([.\w]+)$'
+    android_pref_bib2_rx = '.+preferredResourceId=LLS:([.\w]+)&dataTypeReference=bible'
+    android_reading_plan_rx = '.+readingPlanTitle=([\s\w]+),\s.+'
+    firebase_rx = '.+FirebaseInstanceId.+'
 
     with open(each_iter_file, 'r', encoding='latin-1') as current_log:
+        detected = False
         for line in current_log:
+            if detected:
+                crash.append(line)
+                detected = False
             if search(f'{ios_date_time_rx}{ios_launched1_rx}', line):
                 multi1 = search(f'{ios_date_time_rx}{ios_launched1_rx}', line) 
-                # print(multi1.groups())
                 add_if_new(multi1.group(2), app_name)
                 add_if_new(multi1.group(3), app_v)
                 add_if_new(multi1.group(4), device)
@@ -110,10 +119,8 @@ def get_info(each_iter_file):
                 add_if_new(multi1.group(6), os_v)
                 add_if_new(multi1.group(7), lang)
                 add_if_new(multi1.group(8), user_id)
-                # print(f'multi ONE {app_name}')
             elif search(f'{ios_date_time_rx}{ios_launched2_rx}', line):
                 multi2 = search(f'{ios_date_time_rx}{ios_launched2_rx}', line) 
-                # print(multi2.group(1, 2, 3, 9, 6, 7, 8))
                 add_if_new(multi2.group(2), app_name)
                 add_if_new(multi2.group(3), app_v)
                 add_if_new(multi2.group(9), device)
@@ -121,17 +128,12 @@ def get_info(each_iter_file):
                 add_if_new(multi2.group(6), os_v)
                 add_if_new(multi2.group(7), lang)
                 add_if_new(multi2.group(8), user_id)
-                # print(f'multi TWO {app_name}')
             elif search(f'{android_date_time_rx}{android_launched_rx}', line):
                 android_launched = search(f'{android_date_time_rx}{android_launched_rx}', line)
-                # print(android_launched.groups())
                 add_if_new('Android', os_name)
                 add_if_new(android_launched.group(2), user_id)
-                # print(user_id)
             elif search(f'{ios_date_time_rx}.+{user1_rx}', line):
                 user_equals1 = search(f'{ios_date_time_rx}.+{user1_rx}', line)
-                # print('\n\nUSER1\n\n')
-                # print(user_equals1.groups())
                 add_if_new('iOS', os_name)
                 add_if_new(user_equals1.group(2), user_id)
             elif search(f'{user2_rx}', line):
@@ -140,50 +142,32 @@ def get_info(each_iter_file):
                 print(user_equals2.group(1))
             elif search(f'{ios_date_time_rx}{user_sup_rx}', line):
                 user_sup = search(f'{ios_date_time_rx}{user_sup_rx}', line)
-                # print(f'\n\niOS Sup\n\n{user_sup.groups()}')
                 add_if_new('iOS', os_name)
                 add_if_new(user_sup.group(2), user_id)
             elif search(f'{ios_date_time_rx}{lls1_rx}', line):
                 lls1 = search(f'{ios_date_time_rx}{lls1_rx}', line)
-                # print('\n\nLLS1\n\n')
-                # print(lls1.groups())
                 add_if_new('iOS', os_name)
                 add_if_new(lls1.group(2), user_id)
             elif search(f'{android_date_time_rx}{lls2_rx}', line):
                 android_lls2 = search(f'{android_date_time_rx}{lls2_rx}', line)
-                print('\n\nANDROID LLS2\n\n')
-                print(android_lls2.groups())
                 add_if_new('Android', os_name)
                 add_if_new(android_lls2.group(2), user_id)
-                print(user_id)
             elif search(f'{android_date_time_rx}{lls1_rx}', line):
                 lls_fire = search(f'{android_date_time_rx}{lls1_rx}', line)
-                print('\n\nLLS ANDROID\n\n')
-                print(lls_fire.groups())
+                add_if_new('Android', os_name)
+                add_if_new(lls_fire.group(2), user_id)
             elif search(f'{android_date_time_rx}{android_mod_rx}', line):
                 mod_file = search(f'{android_date_time_rx}{android_mod_rx}', line)
-                print('\n\nMOD ANDROID\n\n')
-                # print(line)
-                print(mod_file.groups())
                 add_if_new('Android', os_name)
                 add_if_new(mod_file.group(2), user_id)
-                # print(user_id)
             elif search(f'{android_date_time_rx}{fire_items_rx}', line):
                 items_fire = search(f'{android_date_time_rx}{fire_items_rx}', line)
                 print('\n\nITEMS ANDROID\n\n')
                 print(items_fire.groups())
-            # elif search(f'{android_lls_rx}', line):
-                # android_lls = search(f'{android_lls_rx}', line)
-                # print(android_lls.group(1))
-                # print(f'PROBABLY ANDROID LLS:\n{line}')
             elif search(f'{ios_date_time_rx}{anon_rx}', line):
                 anon_user = search(f'{ios_date_time_rx}{anon_rx}', line)
-                # print(f'ANON USER:\n{line}')
-                # print(anon_user.groups())
                 add_if_new('iOS', os_name)
                 add_if_new(anon_user.group(2), user_id)
-            # elif search('users', line) or search('Users', line):
-                # print(line)
             elif search(f'{android_date_time_rx}{bep_id_rx}', line):
                 android_bep_id = search(f'{android_date_time_rx}{bep_id_rx}', line)
                 print('\n\nANDROID BEP ID\n\n')
@@ -210,34 +194,76 @@ def get_info(each_iter_file):
             elif search(f'{bep_ios_date_time_rx}{bep_appsup_rx}', line):
                 bep_appsup = search(f'{bep_ios_date_time_rx}{bep_appsup_rx}', line)
                 print(bep_appsup.groups())
-            elif 'crash detected' in line or 'beginning of crash' in line:
-                print('CRASH DETECTED\n\n')
+            elif search(f'{android_crash_rx}', line):
+                add_if_new('Android', os_name)
                 print(line)
-            elif 'readingPlanTitle=' in line:
-                print('READING PLAN\n\n')
-                print(line)
+                detected = True
+            elif search(f'{ios_date_time_rx}{ios_crash_rx}', line):
+                add_if_new('iOS', os_name)
+                crash.append(line)
+            elif search(f'{android_reading_plan_rx}', line):
+                android_reading_plan = search(f'{android_reading_plan_rx}', line)
+                add_if_new(android_reading_plan.group(1), android_reading_plans)
             elif search(f'{android_date_time_rx}{android_lang_rx}', line):
                 android_lang = search(f'{android_date_time_rx}{android_lang_rx}', line)
                 add_if_new('Android', os_name)
                 add_if_new(android_lang.group(2), lang)
-            elif search(f'{ios_date_time_rx}{pref_bib_rx}', line):
-                ios_pref_bib = search(f'{ios_date_time_rx}{pref_bib_rx}', line)
+            elif search(f'{ios_date_time_rx}{ios_pref_bib_rx}', line):
+                ios_pref_bib = search(f'{ios_date_time_rx}{ios_pref_bib_rx}', line)
                 add_if_new('iOS', os_name)
                 add_if_new(f'LLS:{ios_pref_bib.group(2)}', preferred)
-
-
-
-
+            elif search(f'{android_pref_bib1_rx}', line):                
+                android_pref_bib1 = search(f'{android_pref_bib1_rx}', line)
+                add_if_new('Android', os_name)
+                add_if_new(f'LLS:{android_pref_bib1.group(1)}', preferred)
+            elif search(f'{android_pref_bib2_rx}', line):
+                android_pref_bib2 = search(f'{android_pref_bib2_rx}', line)
+                add_if_new('Android', os_name)
+                add_if_new(f'LLS:{android_pref_bib2.group(1)}', preferred)
+            elif search(firebase_rx, line):
+                add_if_new('Android', os_name)
 
     if len(device) > 1:
         longest = max(device, key=len)
         for varient in device:
             if varient != longest:
                 device.remove(varient)
-    # git statusprint(device)  
-    group_sum = f'OS: {os_name} {os_v}\nApp: {app_name} {app_v}\nDevice: {device}\nLanguage: {lang}\nUser ID: {user_id}\nPreferred Bible(s): {preferred}'
-    # print(group_sum)
+    if len(os_name) > 1 and 'Fire Os' in os_name:
+        for name in os_name:
+            if name != 'Fire Os':
+                os_name.remove(name)
+
+    group_sum = [os_name, os_v, app_name, app_v, device, lang, user_id, preferred, android_reading_plans, crash]   
     return group_sum
+
+def organize_results(log_data):
+    for log in log_data:
+        log.sort()
+    # print(log_data)
+    print(f'\nOs: ')
+    print(*log_data[0], sep = ', ') 
+    print(*log_data[1], sep = ', ')  
+    print('\nApp: ')
+    print(*log_data[2], sep = ', ')
+    print(*log_data[3], sep = ', ')
+    print('\nDevice: ')
+    print(*log_data[4], sep = ', ')
+    print('\nLanguage:')
+    print(*log_data[5], sep = ', ')
+    print('\nUser ID(s):')
+    print(*log_data[6], sep = ', ')
+    print('\nPreferred Bible(s):')
+    print(*log_data[7], sep = ', ')
+    print('\nDetected Reading Plans:')
+    print(*log_data[8], sep = ', ')
+    print(f'\n{len(crash)} Crash(es):')
+    print(*log_data[9], sep = '\n')
+
+
+
+
+
+    
 
 unzip_iter()    
 
